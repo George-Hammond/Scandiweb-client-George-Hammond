e8e731439c9a8c0aa7ac86740b1bfd4b2a64d8c0
George Hammond <george.hammond@amalitech.org>
2022-08-09 10:24:28 +0000

    Import error Solved

--- src/components/categories/Card.js
+++ src/components/categories/Card.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import '../styles/index.css';
-import Sweater from '../images/sweater.svg'
+import '../../styles/index.css';
+import Sweater from '../../images/sweater.svg'
 
 class Card extends React.Component{
     render(){
--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../styles/index.css';
+import '../../styles/index.css';
 import Card from './Card';
 
 class Categories extends React.Component{
